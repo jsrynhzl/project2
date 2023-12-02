7eeead298d2e1ba0f373acda43d3776820070a28
Jesse <jzelle.cor@gmail.com>
2023-12-02 17:04:37 -0500

    Fix Orders page bxslider for flavors

--- js/order.js
+++ js/order.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
     $('.bxslider').bxSlider({
-        mode: 'horizontal',
-        moveSlides: 1,
-        slideMargin: 40,
-        infiniteLoop: true,
-        slideWidth: 660,
-        minSlides: 3,
-        maxSlides: 3,
-        speed: 800,
+        mode: 'fade',
+        captions: true,
+        slideWidth: 200,
+        pager: true
     });
 });
