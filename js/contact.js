8131a3b8ea9ca20d51c35a561afc04d210728de0
sally <wykee001@gmail.com>
2023-12-03 09:46:35 -0500

    contact

--- js/contact.js
+++ js/contact.js
@@ -1,110 +1,75 @@
-// const myForm = document.getElementById("form");
-// const first_name = document.getElementById("first_name");
-// const last_name = document.getElementById("last_name");
-// const phone = document.getElementById("phone");
-// const email = document.getElementById("email");
-// const message = document.getElementById("message");
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     first_name.focus();
-// });
-
-// myForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     checkInputs();
-// });
-
-const myForm = document.getElementById("form");
+const form = document.getElementById("form");
 const first_name = document.getElementById("first_name");
 const last_name = document.getElementById("last_name");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 
-document.addEventListener("DOMContentLoaded", () => {
-    first_name.focus();
-});
-
-myForm.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const isFormValid = checkInputs();
 
-    if (isFormValid) {
-        myForm.submit();
-    }
+    validateInputs();
 });
 
-function checkInputs() {
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+const validateInputs = () => {
     const first_nameValue = first_name.value.trim();
     const last_nameValue = last_name.value.trim();
     const phoneValue = phone.value.trim();
     const emailValue = email.value.trim();
 
-    let isFormValid = true;
-
-    if (first_nameValue === "") {
-        setError(first_name, "First name is required.");
-        isFormValid = false;
+    if (first_nameValue === '') {
+        setError(first_name, 'First name is required.');
     } else {
         setSuccess(first_name);
     }
 
-    if (last_nameValue === "") {
-        setError(last_name, "Last name is required.");
-        isFormValid = false;
+    if (last_nameValue === '') {
+        setError(last_name, 'Last name is required.');
     } else {
         setSuccess(last_name);
     }
 
-    if (phoneValue === "") {
-        setError(phone, "Phone number is required.");
-        isFormValid = false;
+    if (phoneValue === '') {
+        setError(phone, 'Phone number is required.');
     } else if (isNaN(phoneValue) || phoneValue.length !== 10 ) {
-        setError(phone, "Must be a valid phone number.");
-        isFormValid = false;
-        phone.focus();
+        setError(phone, 'Must be a valid phone number.');
     } else {
         setSuccess(phone);
     }
 
-    if (emailValue === "") {
-        setError(email, "Email address is required.");
-        isFormValid = false;
-    } else if (!isValid(emailValue)) {
-        setError(email, "Must be a valid email address.");
-        isFormValid = false;
-        email.focus();
+    if (emailValue === '') {
+        setError(email, 'Email address is required.');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Must be a valid email address.');
     } else {
         setSuccess(email);
     }
 
-    function isValid(email) {
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailPattern.test(email)
-    }
+    if (document.querySelectorAll('.success').length === 4) {
+        form.submit();
+    } 
 
-    if (
-        setSuccess(first_name) &&
-        setSuccess(last_name) &&
-        setSuccess(phone) &&
-        setSuccess(email)
-        ) {
-        myForm.submit();
-    }
-
-};
-
-function setError(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-    formControl.classList.add("error");
-    formControl.classList.remove("success");
-};
-
-function setSuccess(input) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    formControl.classList.add("success");
-    formControl.classList.remove("error");
 };
