1357636d0c9724119ca1f52cd493c87d0f0a7156
Jesse <jzelle.cor@gmail.com>
2023-12-03 11:09:04 -0500

    Change photos for Order page and use carousel

--- js/order.js
+++ js/order.js
@@ -6,3 +6,18 @@ $(document).ready(function () {
         pager: true
     });
 });
+
+$(document).ready(function () {
+    $('.carousel').bxSlider({
+        pager: false,
+        controls: false,
+        speed: 60000,
+        useCSS: false,
+        ticker: true,
+        tickerHover: true,
+        slideWidth: 300,
+        responsive: false,
+        minSlides: 1,
+        maxSlides: 4,
+    });
+});
